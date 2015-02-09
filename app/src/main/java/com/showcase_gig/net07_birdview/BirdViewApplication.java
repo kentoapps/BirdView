b4840c5271e18187c5a55f4516a6d64033883bb3
Kento Uchida <kento.uchida@showcase-gig.com>
2015-02-09 21:00:45 +0900

    Add setting of push

--- app/src/main/java/com/showcase_gig/net07_birdview/BirdViewApplication.java
+++ app/src/main/java/com/showcase_gig/net07_birdview/BirdViewApplication.java
@@ -1,8 +1,12 @@
 package com.showcase_gig.net07_birdview;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 /**
  * Created by kento on 15/02/05.
@@ -15,5 +19,16 @@ public class BirdViewApplication extends Application{
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 }
